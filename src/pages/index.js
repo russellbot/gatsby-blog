a538bfc1eaa491afc76797bd92080b37bb1607a5
Russell Hart <russellbot@gmail.com>
2021-04-06 17:13:51 -0400

    added styled components and added links to blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,46 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
+
 export default ({ data }) => {
-  console.log(data);
+  console.log(data)
 
   return (
-  <Layout>
-    <SEO title="Home" />
-    <div>
-      <h1>Russell's Thoughts</h1>
-      <h4>{ data.allMarkdownRemark.totalCount }</h4>
-      {
-        data.allMarkdownRemark.edges.map(({node}) => (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h1>Russell's Thoughts</h1>
+        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <span>{ node.frontmatter.title } - { node.frontmatter.date}</span>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} - {node.frontmatter.date}
+              </BlogTitle>
+            </BlogLink>
             <p>{node.excerpt}</p>
           </div>
-        ))
-      }
-    </div>        
-  </Layout>
-)}
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
@@ -37,7 +49,10 @@ export const query = graphql`
             date
             description
             title
-          }          
+          }
+          fields {
+            slug
+          }
           excerpt
         }
       }
